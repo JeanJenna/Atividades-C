92d419947751595d5a5c942130bbb3d918b03e15
JeanJenna <Jeangriggis0@gmail.com>
2025-10-29 20:52:44 -0400

    Update ReceitasBolo.c

--- ReceitasBolo.c
+++ ReceitasBolo.c
@@ -6,10 +6,10 @@
 
         setlocale(LC_ALL, "");
         int num = 0;
-    char bolo [] = {"1 lata (395 g) de leite condensado \n3 ovos inteiros \n2 colheres (sopa) de manteiga derretida \n1 xícara (chá) de farinha de trigo (120 g) \n1 colher (sopa) de fermento químico em pó (10 g)"};
+    char bolo [] = {"1 lata (395 g) de leite condensado \n3 ovos inteiros \n2 colheres (sopa) de manteiga derretida \n1 xÃ­cara (chÃ¡) de farinha de trigo (120 g) \n1 colher (sopa) de fermento quÃ­mico em pÃ³ (10 g)"};
 
     printf("Para exibir a receita do bolo digite 1 e para sair digite 0: ");
-    scanf("%d", num);
+    scanf("%d", &num);
 
     if (num == 1) {
         printf("%s", bolo);
@@ -23,3 +23,4 @@
 
     return 0;
 }
+
